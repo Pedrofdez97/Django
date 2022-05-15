738cf31d779e4ed57e08a1da74ff781d18a2f3da
Pedro <pfernandezsalas@safareyes.es>
2022-05-15 23:59:04 +0200

    Proyecto terminado Django

--- futbol/urls.py
+++ futbol/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from .views import inicio, jugadores, equipo, crearEquipo, crearJugador
-
+from .views import *
 urlpatterns = [
     path('index/', inicio),
     path('jugadores/<id>', jugadores),
-    path('equipo/', equipo),
-    path('crearequipo/', crearEquipo),
     path('crearjugador/', crearJugador),
+    path('modificar/<int:id>', modifcarJugador),
+    path('eliminar/<id>', eliminarjugador)
 ]
--- futbol/views.py
+++ futbol/views.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import *
 
-
 def inicio(request):
     lista_futbolista = Futbolista.objects.all()
     return render(request, 'index.html', {'juga': lista_futbolista})
 
 
-def equipo(request):
-    return render(request, 'equipo.html')
+def eliminarjugador(request, id):
+    juga = Futbolista.objects.get(id=id)
+    if juga is not None:
+        Futbolista.delete(juga)
+    return redirect('/futbol/index/')
+
 def crearJugador(request):
 
     if request.method == 'POST':
@@ -26,8 +29,21 @@ def crearJugador(request):
         return redirect('/futbol/index/')
     else:
         return render(request, 'crearjugadores.html')
-def crearEquipo(request):
-    return render(request, 'crearEquipo.html')
+
+def modifcarJugador(request,id):
+    if request.method == "GET":
+        j = Futbolista.objects.filter(id=id).first()
+        return render(request, 'modificar.html', {'j': j})
+    else:
+        j = Futbolista.objects.get(id=id)
+        j.nombre = request.POST.get('nombre')
+        j.equipo = request.POST.get('equipo')
+        j.fecha_nacimiento = request.POST.get('fecha_nacimiento')
+        j.fecha_debut = request.POST.get('fecha_debut')
+        j.equipo_debut = request.POST.get('equipo_debut')
+        Futbolista.save(j)
+        return redirect('/futbol/index/')
+
 
 
 def jugadores(request, id):
